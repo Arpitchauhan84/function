f29b2ef9690d905bf042bf3201153a794dbf5332
Arpit chauhan <141748776+Arpitchauhan84@users.noreply.github.com>
2023-09-15 07:55:23 +0000

    function

---
+++ 02_basic.js
@@ -0,0 +1,5 @@
+const user = {
+    username: "arpitchauhan",
+    price:200
+}
+console.log(user.username);
---
+++ function.js
@@ -0,0 +1,70 @@
+// function saymyname(){
+// console.log('a');
+// console.log('r');
+// console.log('p');
+// console.log('i');
+// console.log('t');
+// }
+
+// saymyname()
+
+// function addtwonumber(num1,num2) {
+// return  num1+num2;
+// }
+// let sum=addtwonumber(14,25)
+// console.log("result:",sum);
+
+// function logginmessage(username) {
+//     if(username){
+//         console.log('please enter a username');
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(logginmessage("arpit"))
+
+// function claculate(num1) {
+//     return num1
+// }
+// claculate(2)
+
+// function name(value1,value2,...num1) {
+//     return num1
+// }
+// console.log(name(100,200,400));
+
+// let user={
+//     name:"arpit",
+//     email:"arpitchauahn3855@outlook.com"
+// }
+
+// function print(obj){
+//     console.log(`username ${obj.name} and email:${obj.email}`);
+// }
+// print(user)
+
+// let name=()=>{
+//     console.log('arpit');
+//     for(let i=0;i<12;i++){
+//         console.log(i);
+//     }
+// }
+// name()
+
+// function one(){
+//     const username='arpitchauhan'
+
+// function two(){
+//     const webside='youtube'
+//     console.log(username);
+// }
+// // console.log(webkit);
+// two()
+// }
+// one()
+
+// function name(params) {
+//     return name +1
+// }
+// //console.log(name(4));
+-
+// name(8)
