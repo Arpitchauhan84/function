2d9709bc4d2f916c583ddd5caade8ac8c43c035a
Arpit chauhan <141748776+Arpitchauhan84@users.noreply.github.com>
2023-09-16 05:55:30 +0000

    new changes

--- 02_basic.js
+++ 02_basic.js
@@ -1,5 +1,63 @@
-const user = {
-    username: "arpitchauhan",
-    price:200
-}
-console.log(user.username);
+// const user = {
+//     username: "arpitchauhan",
+//     price:200,
+//     welcome:function(){
+//         console.log(` ${this.username},welcome to webside`);
+//     }
+// }
+// // console.log(welcome())
+// user.welcome()
+
+// let user={
+//     name:"arpit",
+//     email:"arpitchuahan3855@outlook.com",
+
+//     information:function(){
+//         console.log(`welcome to ${this.name}`);
+//         console.log(this);
+//     }
+// }
+// user.information()
+// console.log(this);
+// user.name='prince'
+// console.log(user.name);
+
+// function love() {
+//     let couplename='arpit'
+//     console.log(couplename);       do not decleared this key only function
+// }
+//love()
+
+// let love=function(){
+//     let couplename='arpit'
+//     console.log(this.couplename);
+// }
+// console.log(love);
+
+// let sumtwonumber = (num1,num2) => {
+//     return num1+num2
+// }
+// console.log(sumtwonumber(12,23));
+
+
+// let sumtwonumber = (num1,num2) => num1+num2
+// console.log(sumtwonumber(12,23));
+
+// (function chai(){
+//     console.log('arpit chai');
+// })()
+
+// (let sumtwonumber = (num1,num2) => {
+//     return num1+num2
+// })()
+// console.log(sumtwonumber(12,23));
+
+// (function name(name='prince') {
+//     console.log('arpit',name);
+// })()
+
+
+
+setTimeout(() => {
+    console.log(arpit);
+}, 1);
