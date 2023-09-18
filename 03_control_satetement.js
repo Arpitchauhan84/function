30ed6793d7e157f5367560f532262d651b5e9561
Arpit chauhan <141748776+Arpitchauhan84@users.noreply.github.com>
2023-09-18 05:29:25 +0000

    array_llop.change

---
+++ 03_control_satetement.js
@@ -0,0 +1,66 @@
+// const logginuser=true
+// if(logginuser==false){
+//     let date=new Date
+//     console.log(date);
+// }
+// else{
+//     console.log(`not loggin`);
+// }
+
+// const num=12;
+// if(num<50){
+//     console.log(`grater than`);
+// }
+// else{
+//     console.log(`less than`);
+// }
+
+// const arr=[];
+
+// if(arr.length==0){
+//     console.log(`array is empty`);
+// }\
+
+// const obj={
+
+// }
+// if(Object.keys(obj)==0){
+//     console.log(`onject key is empty`);
+// }
+
+// let iceprice=100
+// iceprice <=80 ? console.log(`less than 100`):console.log(`greater than 100`)
+
+
+// iterstion & lops
+
+// let array=[10,20,30,40,50,60,78,8,9,100];
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+
+// let table=4
+
+// for(let i=1;i<=10;i++){
+//     if(i==4){
+//         break
+//     }
+//     console.log(`${table}*${i}=${table*i}`);
+// }
+
+// let num=997;
+// let i,flage=0;
+// for(i=2;i=num/2;i++){
+//     if(flage==0){
+//         console.log('num is prime');
+//     }
+//     else{
+//         console.log('not a prime');
+//     }
+//     if(flage==1){
+//         console.log('num is prime');
+//     }
+// }
---
+++ array_loop.js
@@ -0,0 +1,73 @@
+// let arr=[1,2,3,4,5]
+// for(const num of arr){
+//     console.log(num);
+// }
+
+// let name=['arpit','prince','thakur']
+
+// for(const anothername  of name){
+//     console.log(anothername);
+// }
+
+// const map=new Map()
+// map.set('in','india')
+// map.set('usa','amerika')
+// map.set('urop','rusia')
+// map.set('uk','london')
+// map.set('in','kolkata')
+
+// console.log(map);
+
+// for (const [iterator,value] of map) {
+//     console.log(iterator,value);
+//}
+
+
+// let obj={
+//     compiler:"c",
+//     game:"c++",
+//     web:"html,css,gavascript",
+//     backup:"java,python"
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//     }
+//     console.log(`i am devolper in uss${key} and ${obj[key]}`);
+// }
+
+// let programmer=['c','c++','java','python']
+
+// for(const key in programmer){
+//     console.log(key[programmer]);
+// }
+// programmer.forEach(function(i){
+//     console.log(i);
+// });
+// programmer.forEach((j) => {
+//     console.log(j);
+// // });
+
+// let numer=[1,2,3,4,5,6]
+
+// numer.filter((num)=>{
+// if(num>4){
+//     console.log(num);
+// }
+// })    
+
+
+// let num=[1,2,3,4,5]
+
+// const nu=num.map((n)=>(n+10))
+// .map((n)=>n*10)
+// console.log(nu);
+
+
+// let book={
+//     let bk= [book1 ="mahabharat",author="rishi ved vyasa", language="sanskrit"]
+// }
+
+// console.log(book);
+
